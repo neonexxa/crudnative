168313ab3269c581da56336c065f7364527066ee
neonexxa <firdaus@cyberhax.club>
2017-02-14 19:03:05 +0800

    rewrite

--- dbsetup.php
+++ dbsetup.php
@@ -17,17 +17,17 @@ class dbsetup extends PHPUnit_Framework_TestCase
     }
     public function testHelloWorld()
     {
-        $helloWorld = new HelloWorld($this->pdo);
+        $helloWorld = new dbsetup($this->pdo);
         $this->assertEquals('Hello World', $helloWorld->hello());
     }
     public function testHello()
     {
-        $helloWorld = new HelloWorld($this->pdo);
+        $helloWorld = new dbsetup($this->pdo);
         $this->assertEquals('Hello Bar', $helloWorld->hello('Bar'));
     }
     public function testWhat()
     {
-        $helloWorld = new HelloWorld($this->pdo);
+        $helloWorld = new dbsetup($this->pdo);
         $this->assertFalse($helloWorld->what());
         $helloWorld->hello('Bar');
         $this->assertEquals('Bar', $helloWorld->what());
